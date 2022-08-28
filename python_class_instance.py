efb81518658068098f02c15722474319064b00dc
bibinvninan32 <105633202+bibinvninan32@users.noreply.github.com>
2022-08-29 00:40:30 +0530

    Update python_class_instance.py
    shows how a class method is used to create a object.

--- python_class_instance.py
+++ python_class_instance.py
@@ -25,11 +25,21 @@ class Employee:
 		def set_raise_percent(cls, percent):
 			cls.raise_percent = percent
 
+		@classmethod
+		def from_string(cls, emp_str):
+			f, l, s = emp_str.split('-')
+			print(cls(f, l, s)) #object is created
+			return cls(f, l, s) #object is returned
+
 
 emp_1 = Employee('sam','kurien',100)
 emp_2 = Employee('winston','churchil',100)
+emp_str3 = 'John-Doe-7000'
+
 
 Employee.set_raise_percent(1.30)
+emp_3 = Employee.from_string(emp_str3)
+
 
 #emp_2.raise_percent = 1.20
 print(Employee.raise_percent)
@@ -40,3 +50,8 @@ print(emp_1.salary)
 print(emp_1.apply_raise())
 print(emp_2.salary)
 print(emp_2.apply_raise())
+#print(emp_3.salary)
+#print(emp_3.apply_raise())
+
+#print(emp_3.salary)
+print(emp_3)
